da5dd90d0cd315b65ccde55d1cedab89b3e657d0
Noor Alhomsi <nalhomsi@gmail.com>
2021-09-27 18:56:16 -0400

    app js

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -10,9 +10,9 @@ function App() {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: 'Photography', description: 'Photos and art' },
+    { name: 'Syria', description: 'Syria - people and places' },
+    { name: 'Web Design', description: 'Webpage design' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
